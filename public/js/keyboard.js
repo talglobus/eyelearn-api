d441c60eb67001659d6b3cf2e47bd8072afd9212
Tal Globus <talglobus@gmail.com>
2017-10-01 12:52:40 -0700

    Prepare for nearing demo

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -2,3 +2,29 @@
  * © 2017 Tal Globus. All Rights Reserved.
  */
 
+
+//
+// $("body").keydown(function( event ) {
+// 	if (event.which === 39) {		// If pressed right
+// 		event.preventDefault();
+// 		$.get( "/next/", function( data ) {
+// 			$( ".result" ).html( data );
+// 			alert( "Load was performed." );
+// 		});
+// 	} else if (event.which === 37) { // If pressed left
+// 		event.preventDefault();
+//
+// 	} else if (event.which === 38) { // If pressed up
+// 		event.preventDefault();
+//
+// 	} else if (event.which === 40) { // If pressed down
+// 		event.preventDefault();
+//
+// 	} else if (event.which === 32) { // If pressed space
+// 		event.preventDefault();
+//
+// 	} else if (event.which === 13) { // If pressed space
+// 		event.preventDefault();
+// 	}
+//
+// });
